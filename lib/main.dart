ade2894d54b3e33ec1b60cf6e640941cfe957105
Isaias Medina <isamed92@gmail.com>
2023-05-09 22:41:35 -0300

    notification bloc added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:push_notifications/config/router/app_router.dart';
 import 'package:push_notifications/config/theme/app_theme.dart';
+import 'package:push_notifications/presentation/blocs/notification_bloc/notifications_bloc.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MultiBlocProvider(providers: [
+    BlocProvider(
+      create: (context) => NotificationsBloc(),
+    )
+  ], child: const MainApp()));
 }
 
 class MainApp extends StatelessWidget {
